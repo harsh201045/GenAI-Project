36c6836962a2f90903e741165872ef79f996ccd3
Harsh Pabani <119148644+harsh201045@users.noreply.github.com>
2026-09-08 02:11:12 +0530

    Update CORS origin to production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://genai-project-9ezq.onrender.com",
     credentials: true
 }))
 
